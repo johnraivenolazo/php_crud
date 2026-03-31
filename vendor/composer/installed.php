7d8440c20443ff8ce5d50dd3a1cc9d51f47dcdbc
johnraivenolazo <johnraivenolazo@gmail.com>
2026-03-31 21:35:42 +0800

    add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aven/php-crud-pdo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '579d9846e26d998f218ea90a88a64be9bacbb491',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'aven/php-crud-pdo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '579d9846e26d998f218ea90a88a64be9bacbb491',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
